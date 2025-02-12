95da9cb625b62ffb565c171fc6e80b16c3835595
Сергей Щ <actek92@gmail.com>
2025-02-12 14:00:29 +0700

    add final

---
+++ Module/src/Poem.java
@@ -0,0 +1,9 @@
+// Напиши программу, которая выводит несколько строк текста, например, стихотворение или цитату.
+public class Poem {
+    public static void main(String[] args){
+        System.out.println("У лукоморья дуб зеленый,");
+        System.out.println("Златая цепь на дубе том:");
+        System.out.println("И днем и ночью кот ученый");
+        System.out.println("Все ходит по цепи кругом.");
+    }
+}
